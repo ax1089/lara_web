d5842c306959992f058cfbc7980e05b32441f38a
wencai159@126.com <wencai159@126.com>
2021-11-28 03:38:55 +0800

    话题排序

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,9 +9,9 @@ use Illuminate\Http\Request;
 class CategoriesController extends Controller
 {
     //
-    public function show(Category $category){
+    public function show(Category $category,Request $request,Topic $topic){
         //读取分类ID 关联的话题，并按每 20条分页
-        $topics = Topic::where('category_id',$category->id)->paginate(20);
+        $topics = $topic->withOrder($request->order)->where('category_id',$category->id)->with('user','category')->paginate(20);
         //传参变量话题和分类到模版中
         return view('topics.index',compact('topics','category'));
     }
